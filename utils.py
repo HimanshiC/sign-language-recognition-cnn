58a072e9e6ee4bd4da82d966433f815c677b3292
HimanshiC <159806017+HimanshiC@users.noreply.github.com>
2024-12-09 16:36:15 +0530

    Update utils.py
    organized the code blocks and labelled them

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
+
+#CNN architecture with functions for training and validation
 import torch
 import torch.nn as nn
 
+#Convolutional Block definition
 class MyConvBlock(nn.Module):
     def __init__(self, in_ch, out_ch, dropout_p):
         kernel_size = 3
         super().__init__()
-
+        #Initialization
         self.model = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size, stride=1, padding=1),
             nn.BatchNorm2d(out_ch),
@@ -17,12 +20,13 @@ class MyConvBlock(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+#Accuracy function code
 def get_batch_accuracy(output, y, N):
     pred = output.argmax(dim=1, keepdim=True)
     correct = pred.eq(y.view_as(pred)).sum().item()
     return correct / N
 
-
+#training funcion code
 def train(model, train_loader, train_N, random_trans, optimizer, loss_function):
     loss = 0
     accuracy = 0
@@ -38,7 +42,7 @@ def train(model, train_loader, train_N, random_trans, optimizer, loss_function):
         loss += batch_loss.item()
         accuracy += get_batch_accuracy(output, y, train_N)
     print('Train - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
-
+#validation function code
 def validate(model, valid_loader, valid_N, loss_function):
     loss = 0
     accuracy = 0
